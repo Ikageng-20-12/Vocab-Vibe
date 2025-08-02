ac53150a2673ae4ae95314bdbc73855f6489f565
Ikageng <fthitane@gmail.com>
2025-08-02 23:26:14 +0200

    Modified The sb sline file to handle keys securely

--- vocab-vibe/src/util/supabaseClient.ts
+++ vocab-vibe/src/util/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hgspdrbdeyxqesykoseu.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhnc3BkcmJkZXl4cWVzeWtvc2V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzNzY5MzYsImV4cCI6MjA1Mzk1MjkzNn0.VUEspcYgnkapnxVdlIYdaSOULM1uloQ78FaF_tQwTuo';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
